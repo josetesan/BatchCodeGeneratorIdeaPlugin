d9a1278845a48a6d83ddb0c619670e0ca1e66398
jose luis sanchez <muallin@gmail.com>
2015-06-19 19:23:59 +0200

    Initial commit

---
+++ src/org/josete/idea/plugins/BatchJavaGenerator.java
@@ -0,0 +1,13 @@
+package org.josete.idea.plugins;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by josete on 19/6/15.
+ */
+public class BatchJavaGenerator extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
